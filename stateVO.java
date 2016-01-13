a864e6580148d465dfeffeaf2a62b421f1d0e9f3
Shalin amin <shalinamin826@gmail.com>
2016-01-13 19:10:10 +0530

    Create stateVO.java

---
+++ stateVO.java
@@ -0,0 +1,35 @@
+package vo;
+
+import java.io.Serializable;
+
+public class StateVO implements Serializable {
+
+	private int stateId;
+	private String stateName;
+	private CountryVO countryVO;
+
+	public int getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(int stateId) {
+		this.stateId = stateId;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
+	public CountryVO getCountryVO() {
+		return countryVO;
+	}
+
+	public void setCountryVO(CountryVO countryVO) {
+		this.countryVO = countryVO;
+	}
+
+}
