d7241b3f6153d732a667b9424ce859272baff417
Shalin amin <shalinamin826@gmail.com>
2016-01-13 19:09:47 +0530

    Create CountryVO.java

---
+++ CountryVO.java
@@ -0,0 +1,25 @@
+package vo;
+
+import java.io.Serializable;
+
+public class CountryVO implements Serializable {
+	private int countryId;
+	private String countryName;
+
+	public int getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+}
